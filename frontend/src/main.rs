4a7fde5a50ebb8f637b79f9121b45592b3f4b728
fluxoz <john@coyote.technology>
2026-03-18 14:48:35 -0700

    latest

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,8 +2,10 @@ mod app;
 mod api;
 mod models;
 mod components;
+use log::info;
 
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     yew::Renderer::<app::App>::new().render();
 }

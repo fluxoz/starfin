8d12f330fce5d44fb48fa41d83fee6efe05303af
fluxoz <john@coyote.technology>
2026-03-23 14:23:05 -0700

    removed

--- dash.js/samples/modules/typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as dashjs from 'dashjs';
-
-let url = "https://dash.akamaized.net/envivio/Envivio-dash2/manifest.mpd";
-let player = dashjs.MediaPlayer().create();
-// @ts-ignore
-player.initialize(document.querySelector('#myMainVideoPlayer'), url, true);
-let version = player.getVersion();
-document.getElementById('version-output').innerText = `Version ${version}`;
